2ee2c713d61d85d03e6e517998e6b8ca7b1d0715
Omar Hernández <127421759+omar15hr@users.noreply.github.com>
2025-03-27 12:46:29 -0300

    inicio de proyecto y instalaciones pertinentes

---
+++ src/plugins/TanStackProvider.tsx
@@ -0,0 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const TanStackProvider = ({ children }: Props) => {
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    </>
+  );
+};
